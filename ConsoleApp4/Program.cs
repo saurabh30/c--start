3692ab46d515dcb9d4e1c6af6766a6776acc8027
saurabh30 <gupta.saurabh711@gmail.com>
2018-09-05 13:25:55 +0530

    add view completed

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -6,10 +6,40 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp4
 {
+    class Employee
+    {
+        public int eID;
+        public String name;
+
+        public Employee(int eID, String name) {
+            this.eID = eID;
+            this.name = name;
+        }
+
+    }
+
+    static class EmployeeList
+    {
+        static List<Employee> list = new List<Employee>();
+
+        public static void add(Employee e) {
+            list.Add(e);
+        }
+
+        public static void view() {
+            foreach (var employee in list) {
+                Console.WriteLine(employee.eID+" "+employee.name);
+            }  
+        } 
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            EmployeeList.add(new Employee(319403,"saurabh"));
+            EmployeeList.add(new Employee(319406, "urabh"));
+            EmployeeList.view();
         }
     }
 }
